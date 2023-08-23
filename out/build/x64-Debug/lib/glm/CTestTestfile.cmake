1dd227759c6e6d667189ff180286279d87ad0908
Marcelo Martins <marcelo-martins@defensoria.rs.def.br>
2023-08-23 01:01:42 -0300

    chore: apply .gitignore

--- out/build/x64-Debug/lib/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marce/UFRGS/inverse-kinematics/lib/glm
-# Build directory: C:/Users/marce/UFRGS/inverse-kinematics/out/build/x64-Debug/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
